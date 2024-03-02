dcd101ea189254248b397568dfa89192fe82e6a8
deep-priyo <129691885+deep-priyo@users.noreply.github.com>
2024-03-02 17:25:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var rand = Math.random();
 rand = (Math.floor(rand*6))+1;
-var img1src = "./images/dice"+rand+".png";
+var img1src = "dice"+rand+".png";
 
 var img1 = document.querySelectorAll("img")[0];
 img1.setAttribute("src", img1src);
 
 var rand2 = Math.random();
 rand2 = (Math.floor(rand2*6))+1;
-var img2src="./images/dice"+rand2+".png";
+var img2src="dice"+rand2+".png";
 
 var img2= document.querySelectorAll("img")[1];
 img2.setAttribute("src",img2src);
