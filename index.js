4652acf554914cf450ca70c31cb82f6b2fbc9262
deep-priyo <129691885+deep-priyo@users.noreply.github.com>
2024-03-02 17:12:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var rand = Math.random();
+rand = (Math.floor(rand*6))+1;
+var img1src = "./images/dice"+rand+".png";
+
+var img1 = document.querySelectorAll("img")[0];
+img1.setAttribute("src", img1src);
+
+var rand2 = Math.random();
+rand2 = (Math.floor(rand2*6))+1;
+var img2src="./images/dice"+rand2+".png";
+
+var img2= document.querySelectorAll("img")[1];
+img2.setAttribute("src",img2src);
+if(rand>rand2)
+{
+     document.querySelector("h1").innerHTML="🚩Player 1 Wins";
+    
+}
+if(rand<rand2)
+{
+     document.querySelector("h1").innerHTML="🚩Player 2 Wins";
+    
+}
+if(rand==rand2)
+{
+    document.querySelector("h1").innerHTML="Draw";
+}
+
